599da9ecaaee25899e715aa61d7df42fd1ada580
Gabriel Erazo <gfranciscoerazom@protonmail.com>
2023-11-08 12:46:22 -0500

    Create dino animations

--- Assets/Scripts/Dinosaur.cs
+++ Assets/Scripts/Dinosaur.cs
@@ -8,17 +8,20 @@ public class Dinosaur : MonoBehaviour
     [SerializeField] private float radius;
 
     private Rigidbody2D characterRigidbody2D;
+    private Animator dinoAnimator;
 
     // Start is called before the first frame update
     void Start()
     {
         characterRigidbody2D = GetComponent<Rigidbody2D>();
+        dinoAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         bool isGrounded = Physics2D.OverlapCircle(groundCheck.position, radius, ground);
+        dinoAnimator.SetBool("IsGrounded", isGrounded);
         
         if (Random.Range(0, 500) < 5)
         {
